27bd5c2ef1575224424fe9a13ce607ae1ab87757
klaute <klaute@gmail.com>
2018-04-07 21:19:43 +0200

    First version which can connect to an accesspoint. The webserver is also available.

--- firmware/config.h
+++ firmware/config.h
@@ -1,13 +1,14 @@
 
-#define DEBUG 1
+#define DEBUG 1 
 
 #define STATUS_LED_PIN        BUILTIN_LED
 
-#define MX1_PIN               D0
+#define MS1_PIN               D0
 #define MS2_PIN               D1
 #define MS3_PIN               D2
 #define STEP_PIN              D3
 #define DIR_PIN               D4
 
 String DEVICE_NAME            = "turntable";
+
 
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -13,24 +13,28 @@
  * 
  */
 
-uint8_t  mx_bm       = 0x00;
-uint16_t delay_ms_u8 = 100;
-boolean  dir_b       = true;
-boolean  enabled_b   = true;
+uint8_t  ms_bm        = 0x00;
+uint16_t delay_ms_u16 = 100;
+boolean  dir_b        = true;
+boolean  enabled_b    = true;
 
 WiFiManager wifiManager;
-WiFiServer wifiServer;
+WiFiServer server(80); //Initialize the server on Port 80
 
-void setup() { 
-  #ifdef DEBUG
-    Serial.begin(115200);
-  #endif
+void setup()
+{
 
+#ifdef DEBUG
+  Serial.begin(115200);
+#endif
+
+  digitalWrite(STATUS_LED_PIN, HIGH);
+  
   // Pin settings
   pinMode(STATUS_LED_PIN, OUTPUT);
-  pinMode(MX1_PIN, OUTPUT);
-  pinMode(MX2_PIN, OUTPUT);
-  pinMode(MX3_PIN, OUTPUT);
+  pinMode(MS1_PIN, OUTPUT);
+  pinMode(MS2_PIN, OUTPUT);
+  pinMode(MS3_PIN, OUTPUT);
   pinMode(STEP_PIN, OUTPUT);
   pinMode(DIR_PIN, OUTPUT);
 
@@ -44,46 +48,60 @@ void setup() {
       ESP.reset();
       delay(1000);
   }
-  #ifdef DEBUG
-    Serial.println("Connected!");
-  #endif
+#ifdef DEBUG
+  Serial.println("Connected!");
+#endif
 
   delay(100);
+
+  server.begin(); // Start the HTTP Server
 
   digitalWrite(STATUS_LED_PIN, LOW);
 }
 
-void loop() {
+void loop()
+{
   
   WiFiClient client = server.available();
   if (client)
   { 
     // TODO process the webserver settings (parallalize it)
     String request = client.readStringUntil('\r');
-    if (request.indexOf("/EN") != -1){ 
+    if (request.indexOf("/EN") != -1)
+    {
+      Serial.println("Enable rotation");
       enabled_b = true;
-    else if (request.indexOf("/DIS") != -1){ 
+    } else if (request.indexOf("/DIS") != -1)
+    {
+      Serial.println("Disable rotation");
       enabled_b = false;
     }
   } 
 
   digitalWrite(STATUS_LED_PIN, LOW);
 
-  #ifdef DEBUG
-    //Serial.println("State: " + String() + " hPa");
-  #endif
+#ifdef DEBUG
+  static uint16_t time_ms_u16 = 0;
+  if (time_ms_u16 > 2000)
+  {
+    time_ms_u16 = 0;
+    Serial.println("Status: " + String(enabled_b) + " " + String(ms_bm) + " " + String(dir_b) + " " + String(delay_ms_u16));
+  }
+  time_ms_u16 += delay_ms_u16;
+#endif
 
-  digitalWrite(MX1_PIN,  ((mx_bm & 0x01)       ? HIGH : LOW));
-  digitalWrite(MX2_PIN, (((mx_bm & 0x01) >> 1) ? HIGH : LOW));
-  digitalWrite(MX3_PIN, (((mx_bm & 0x02) >> 2) ? HIGH : LOW));
+  digitalWrite(MS1_PIN,  ((ms_bm & 0x01)       ? HIGH : LOW));
+  digitalWrite(MS2_PIN, (((ms_bm & 0x01) >> 1) ? HIGH : LOW));
+  digitalWrite(MS3_PIN, (((ms_bm & 0x02) >> 2) ? HIGH : LOW));
 
   digitalWrite(DIR_PIN, (dir_b == true ? HIGH : LOW));
 
-  if (enabled_b = true)
+  if (enabled_b == true)
   {
     digitalWrite(STEP_PIN, HIGH);
-    delay(delay_ms_u8);
+    delay(delay_ms_u16);
     digitalWrite(STEP_PIN, LOW);
   }
 }
+
 
